eb5189196a04ffb2810063751a1100d35791f576
Alok Singh Shann <88611199+aloksingh09@users.noreply.github.com>
2024-05-17 08:27:37 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/aloksingh09",
   resume:
-    "https://drive.google.com/file/d/1aUHC6NwhLUJWZL7DVtTpEuWQgOHNRquI/view?usp=drive_link",
+    "https://drive.google.com/file/d/1JR9NBv99h4vkjCDg78oRcV2UyFyFABKB/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/alok-raj-933448219/",
   insta: "https://www.instagram.com/aloksinghshann/",
 };
